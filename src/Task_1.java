f848ef07c502f4f1f45f0d3f458508934f8651c7
Svidic111 <svidovyi111@gmail.com>
2018-12-05 00:39:52 +0200

    symplifying

--- src/Task_1.java
+++ src/Task_1.java
@@ -19,10 +19,7 @@ public class Task_1 {
         int c = (int) Math.ceil(k);
 
         if (n == a * a) matchesNumber = 2 * a * (a + 1);
-
-        if (k > 0 & k < 1) matchesNumber = 2 * a * (a + 1) + 2 * b + 1;
-
-        if (c >= 1 & c < 4) matchesNumber = 2 * a * (a + 1) + 2 * b + c;
+        else matchesNumber = 2 * a * (a + 1) + 2 * b + c;
 
 
         System.out.println("The minimum number of matches to make " + n + " squares with one-match side equals " + matchesNumber);
